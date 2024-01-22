5a27c04a5d3a1e7f2ae4d0a7c0b75ad83a9bbb61
sherry-kaikai <2398266713@qq.com>
2024-01-22 15:59:37 +0800

    add thread for yolo,is faster

---
+++ 2_python_multiprocess_thread/readme.md
@@ -0,0 +1,13 @@
+本文件夹中的代码不用ipc进行数据传递。
+16路：单独开4个进程，每个进程处理4batch，multidecoder add 4channel。进程里面用线程push。
+
+把engine_image_pre_process.PushImage等push和get分别放入线程，然后另一个线程get 。这样会不会更快？ 
+
+传入参数batch_size：模型batch，用于计算跑几个进程。
+传入参数video_nums：视频路数。process-nums = video_nums除以模型的batchsize
+传入参数loops：每一路视频跑多少张图片后停止。比如传入video_nums=16，loop = 100，总共跑video_nums* loops = 16* 100 = 1600张图片。
+
+
+- 测试数据 
+    - SE5 车牌模型（单类识别） 4batch int8，16路视频，loops=1000。fps=147.8,tpu=100%.
+
---
+++ 2_python_multiprocess_thread/thread_yolo.py
@@ -0,0 +1,257 @@
+import sophon.sail as sail
+import numpy as np
+import threading
+import time
+import os
+import queue
+from multiprocessing import Process
+import argparse
+import logging
+
+class MultiDecoderThread(object):
+    def __init__(self, tpu_id, video_list, resize_type:sail.sail_resize_type, max_que_size:int, loop_count:int,process_id:int):
+        self.channel_list = {}
+        self.tpu_id = tpu_id
+        self.break_flag = False
+        self.resize_type = resize_type
+        self.multiDecoder = sail.MultiDecoder(15, tpu_id)
+        self.multiDecoder.set_local_flag(True)
+
+        self.loop_count = loop_count
+
+        self.post_que = queue.Queue(max_que_size)
+        self.image_que = queue.Queue(max_que_size)
+
+        self.exit_flag = False
+        self.flag_lock = threading.Lock()
+        
+        self.process_id = process_id
+
+        for video_name in video_list:
+            channel_index = self.multiDecoder.add_channel(video_name,1)
+            print("Process {}  Add Channel[{}]: {}".format(process_id,channel_index,video_name))
+            self.channel_list[channel_index] = video_name
+
+        self.alpha_beta = (1.0/255,0),(1.0/255,0),(1.0/255,0)
+    
+    def get_exit_flag(self):
+        self.flag_lock.acquire()
+        flag_temp = self.exit_flag
+        self.flag_lock.release()
+        return flag_temp
+
+    def InitProcess(self, bmodel_name, process_id,dete_threshold,nms_threshold):
+        self.process_id = process_id
+        self.engine_image_pre_process = sail.EngineImagePreProcess(bmodel_name, self.tpu_id, 0)
+        self.engine_image_pre_process.InitImagePreProcess(self.resize_type, True, 10, 10)
+        self.engine_image_pre_process.SetPaddingAtrr()
+        self.engine_image_pre_process.SetConvertAtrr(self.alpha_beta)
+        self.net_w = self.engine_image_pre_process.get_input_width()
+        self.net_h = self.engine_image_pre_process.get_input_height()
+
+        output_names = self.engine_image_pre_process.get_output_names()
+        self.batch_size = self.engine_image_pre_process.get_output_shape(output_names[0])[0]
+        output_shapes = [self.engine_image_pre_process.get_output_shape(i) for i in output_names]
+        logging.debug('Process {},YOLO RECEIVE IPC INIT DONE,bmodel output_shapes {}'.format(self.process_id,output_shapes))
+        self.yolov5_post_async = sail.algo_yolov5_post_cpu_opt_async([output_shapes[0],output_shapes[1],output_shapes[2]],640,640,10)
+        self.bmcv = sail.Bmcv(sail.Handle(0))
+
+        thread_preprocess = threading.Thread(target=self.decoder_and_pushdata, args=(self.channel_list, self.multiDecoder, self.engine_image_pre_process))
+        thread_inference = threading.Thread(target=self.Inferences_thread, args=(self.resize_type, self.tpu_id, self.post_que, self.image_que))
+        thread_postprocess = threading.Thread(target=self.post_process, args=(self.post_que, dete_threshold, nms_threshold))
+        thread_drawresult = threading.Thread(target=self.draw_result_thread, args=(self.image_que,))
+        thread_drawresult.start()
+        thread_postprocess.start()
+        thread_preprocess.start()
+        thread_inference.start()
+       
+    
+    def decoder_and_pushdata(self,channel_list, multi_decoder, PreProcessAndInference):
+        image_index = 0
+        time_start = time.time()
+        total_count = 0
+        while True:
+            if self.get_exit_flag():
+                    break
+            for key in channel_list:
+                if self.get_exit_flag():
+                    break
+                bmimg = sail.BMImage()
+                ret = multi_decoder.read(int(key),bmimg)
+                if ret == 0:
+                    image_index += 1
+                    PreProcessAndInference.PushImage(int(key),image_index, bmimg)
+                    total_count += 1
+                    if total_count == 2000:
+                        total_time = (time.time()-time_start)*1000
+                        avg_time = total_time/total_count
+                        print("########################avg time: {:.2f}".format(avg_time))
+                        total_count = 0
+
+                else:
+                    time.sleep(0.01)
+
+        print("decoder_and_pushdata thread exit!")
+
+    def Inferences_thread(self, resize_type:sail.sail_resize_type, device_id:int, post_queue:queue.Queue, img_queue:queue.Queue):
+        while True:
+            if self.get_exit_flag():
+                break
+            start_time = time.time()
+            output_tensor_map, ost_images, channel_list ,imageidx_list, padding_atrr = self.engine_image_pre_process.GetBatchData(True)
+            width_list = []
+            height_list= []
+            for index, channel in enumerate(channel_list):
+                width_list.append(ost_images[index].width())
+                height_list.append(ost_images[index].height())
+
+            while post_queue.full():
+                time.sleep(0.01)
+                if self.get_exit_flag():
+                    break
+                continue
+            post_queue.put([output_tensor_map,
+                            channel_list,
+                            imageidx_list,
+                            width_list, 
+                            height_list, 
+                            padding_atrr],False)
+            
+            for index, channel in enumerate(channel_list):
+                if self.get_exit_flag():
+                    break
+                while img_queue.full():
+                    time.sleep(0.01)
+                    if self.get_exit_flag():
+                        break
+                    continue 
+                img_queue.put(ost_images[index])
+
+            end_time = time.time()
+            print("GetBatchData time use: {:.2f} ms".format((end_time-start_time)*1000))
+        
+        print("Inferences_thread thread exit!")
+
+
+    def post_process(self, post_quque:queue.Queue, dete_threshold:float, nms_threshold:float):
+        while (True):
+            if self.get_exit_flag():
+                break
+            if post_quque.empty():
+                time.sleep(0.01)
+                continue
+            output_tensor_map, channels ,imageidxs, ost_ws, ost_hs, padding_atrrs = post_quque.get(True)
+
+            dete_thresholds = np.ones(len(channels),dtype=np.float32)
+            nms_thresholds = np.ones(len(channels),dtype=np.float32)
+            dete_thresholds = dete_threshold*dete_thresholds
+            nms_thresholds = nms_threshold*nms_thresholds
+            while True:
+                if self.get_exit_flag():
+                    break
+                ret = self.yolov5_post_async.push_data(channels, imageidxs, output_tensor_map, dete_thresholds, nms_thresholds, ost_ws, ost_hs, padding_atrrs)
+                if ret == 0:
+                    break
+                else:
+                    print("push_data failed, ret: {}".format(ret))
+                    time.sleep(0.01)
+                break
+        print("post_process thread exit!")
+    
+    def draw_result_thread(self, img_queue:queue.Queue):
+        handle = sail.Handle(self.tpu_id)
+        bmcv = sail.Bmcv(handle)
+        total_count = 0
+        start_time = time.time()
+        while (True):
+            if img_queue.empty():
+                time.sleep(0.01)
+                continue
+            ocv_image = img_queue.get(True)
+            objs, channel, image_idx = self.yolov5_post_async.get_result_npy()
+            for obj in objs:
+                logging.info("Process %d,YOLO postprocess DONE! objs:tuple[left, top, right, bottom, class_id, score] :%s",self.process_id,obj)
+
+                # bmcv.rectangle(ocv_image, obj[0], obj[1], obj[2]-obj[0], obj[3]-obj[1],(0,0,255),2)
+            # image = sail.BMImage(handle,ocv_image.height(),ocv_image.width(),sail.Format.FORMAT_YUV420P,sail.ImgDtype.DATA_TYPE_EXT_1N_BYTE)
+            # bmcv.convert_format(ocv_image,image)
+            # for obj in objs:
+            #     txt_d = "{}".format(obj[4])
+            #     bmcv.putText(image, txt_d , obj[0], obj[1], [0,0,255], 1.4, 2)
+            # bmcv.imwrite("{}_{}.jpg".format(channel,image_idx),image)
+
+            total_count += 1
+            if self.loop_count <=  total_count:
+                break
+        end_time = time.time()
+        time_use = (end_time-start_time)*1000
+        avg_time = time_use/self.loop_count
+
+        print("Process {}:Total images: {} ms".format(self.process_id, self.loop_count))
+        print("Total time use: {} ms".format(time_use))
+        print("Avg time use: {} ms".format(avg_time))
+        print("Process {}: {} FPS".format(self.process_id, 1000/avg_time))
+        print("Result thread exit!")
+
+        logging.info("Process {}:Loops{},Total time use: {} ms, avg_time{}, {} FPS".format(self.process_id, self.loop_count,time_use,avg_time,1000/avg_time))
+
+        
+        self.flag_lock.acquire()
+        self.exit_flag = True
+        self.flag_lock.release()
+
+def process_demo(tpu_id, max_que_size, video_name_list, bmodel_name, loop_count, process_id,dete_threshold,nms_threshold):
+    process =  MultiDecoderThread(tpu_id, video_name_list, sail.sail_resize_type.BM_RESIZE_TPU_NEAREST, max_que_size, loop_count,process_id)
+    process.InitProcess(bmodel_name,process_id,dete_threshold,nms_threshold)
+
+
+def argsparser():
+    parser = argparse.ArgumentParser(prog=__file__)
+    parser.add_argument('--multidecode_max_que_size', type=int, default=16, help='multidecode queue')
+    parser.add_argument('--ipc_recive_queue_len', type=int, default=16, help='ipc recive queue')
+    parser.add_argument('--chip_mode', type=str, default='1684x', help='1684x or 1684')
+    parser.add_argument('--video_nums', type=int, default=16, help='procress nums of input')
+    parser.add_argument('--batch_size', type=int, default=4, help='video_nums/batch_size is procress nums of process and postprocess')
+    parser.add_argument('--loops', type=int, default=1000, help='process loops for one video')
+    parser.add_argument('--input', type=str, default='/data/licenseplate_640516-h264.mp4', help='path of input, must be video path') 
+    parser.add_argument('--yolo_bmodel', type=str, default='../models/yolov5s-licensePLate/BM1684X/yolov5s_v6.1_license_3output_int8_4b.bmodel', help='path of bmodel')
+    parser.add_argument('--dev_id', type=int, default=0, help='tpu id')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+
+    args = argsparser()
+
+    if args.chip_mode == '1684':
+        args.yolo_bmodel = '../models/yolov5s-licensePLate/BM1684/yolov5s_v6.1_license_3output_int8_4b.bmodel'
+    logging.basicConfig(filename= f'{args.chip_mode}_without_ipc_process_is_{args.video_nums}.log',filemode='w',level=logging.DEBUG)
+
+    # decoder_count = 4           #每个进程解码的路数
+    max_que_size = args.multidecode_max_que_size           #缓存的大小
+    loop_count = args.loops           #每个进程处理图片的数量，处理完毕之后会退出。
+
+    
+    '''
+    多路视频流
+    '''
+    process_nums = int(args.video_nums/args.batch_size)
+    input_videos = [args.input for _ in range(int(args.video_nums/process_nums))]
+
+    dete_threshold,nms_threshold = 0.65,0.65
+
+    decode_yolo_processes = [Process(target=process_demo,args=(args.dev_id, max_que_size, input_videos, args.yolo_bmodel, loop_count, i,dete_threshold,nms_threshold)) for i in range(process_nums) ]
+    for i in decode_yolo_processes:
+        i.start()
+        logging.debug('start decode and yolo process')
+    start_time = time.time()
+
+    logging.info(start_time)
+    for i in decode_yolo_processes:
+        i.join()
+        logging.debug('DONE decode and yolo process')
+
+    total_time = time.time() - start_time
+    logging.info('video nums{}, process is {},total time is {},loops for one process is {},total fps is {}'.format(args.video_nums,process_nums,total_time,loop_count,(loop_count*process_nums)/total_time))
+
