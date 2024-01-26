54f275d1f796fc917e8aa8102ec4170739b65486
sherry-kaikai <2398266713@qq.com>
2024-01-26 17:56:59 +0800

    add but bug: add remove pics but get ost img cost 100ms
    video 4,process_nums 1: its ok ,fps90
    but when video 16,process 4: ion alloc failed ,cpu is overload ,not ok.

--- 2_python_multiprocess_thread/thread_yolo.py
+++ 2_python_multiprocess_thread/thread_yolo.py
@@ -36,27 +36,31 @@ import signal
 #         except Exception as e:
 #             print(f"无法终止子进程 {pid}：{e}")
 
+
+
 class MultiDecoderThread(object):
-    def __init__(self, draw_images:bool, tpu_id:int, video_list:list, resize_type:sail.sail_resize_type, max_que_size:int, loop_count:int,process_id:int):
+    def __init__(self, draw_images:bool, remove_duplicate_plates:bool, tpu_id:int, video_list:list, resize_type:sail.sail_resize_type, max_que_size:int, loop_count:int, process_id:int):
         self.draw_images = draw_images
+        self.remove_duplicate_plates = remove_duplicate_plates
         self.loop_count = loop_count
-        self.channel_list = {}
         self.tpu_id = tpu_id
         self.process_id = process_id
 
         self.resize_type = resize_type
         self.resize_type_lprnet = sail.sail_resize_type.BM_RESIZE_TPU_LINEAR
-
+        
         self.multiDecoder = sail.MultiDecoder(16, tpu_id)
         self.multiDecoder.set_local_flag(True)
 
         self.post_que = queue.Queue(max_que_size)
         self.image_que = queue.Queue(max_que_size)
-        self.ostimg_box_queue = queue.Queue(max_que_size-4)
+        self.yolo_res_queue = queue.Queue(max_que_size)
+        self.lp_res_queue = queue.Queue(max_que_size+8)
 
         self.exit_flag = False
         self.flag_lock = threading.Lock()
-        
+
+        self.channel_list = {}
         for video_name in video_list:
             channel_index = self.multiDecoder.add_channel(video_name,0)
             print("Process {}  Add Channel[{}]: {}".format(process_id,channel_index,video_name))
@@ -72,6 +76,13 @@ class MultiDecoderThread(object):
             tuple(x * 1 * 0.0078125 for x in [1, -127.5]),
             tuple(x * 1 * 0.0078125 for x in [1, -127.5])
         )
+        
+        # # 车牌上报逻辑
+        # self.template_infos = {} # {"lp_res":{"in":0,"out":0}}
+        # self.template_in_threshold = 3
+        # self.template_out_thresh = 20 
+        # # self.rm_list = []
+        # self.up_list = []
 
     def get_exit_flag(self):
         self.flag_lock.acquire()
@@ -113,17 +124,17 @@ class MultiDecoderThread(object):
         thread_preprocess = threading.Thread(target=self.decoder_and_pushdata, args=(self.channel_list, self.multiDecoder, self.engine_image_pre_process))
         thread_inference = threading.Thread(target=self.Inferences_thread, args=(self.post_que, self.image_que))
         thread_postprocess = threading.Thread(target=self.post_process, args=(self.post_que, dete_threshold, nms_threshold))
-        thread_lprnet = threading.Thread(target=self.lprnet_pre_and_process,args=(self.image_que,self.ostimg_box_queue))
-        thread_drawresult = threading.Thread(target=self.lprnet_post_and_draw_result, args=(self.ostimg_box_queue,))
-        
+        thread_lprnet = threading.Thread(target=self.lprnet_pre_and_process,args=(self.image_que,self.yolo_res_queue))
+        thread_lprnet_post = threading.Thread(target=self.lprnet_post, args=(self.lp_res_queue,))
+        thread_remove_repeat_and_draw = threading.Thread(target=self.remove_repeat_and_draw,args=(self.lp_res_queue,self.yolo_res_queue))
         
         thread_postprocess.start()
         thread_preprocess.start()
         thread_lprnet.start() 
         thread_inference.start() 
-        thread_drawresult.start()
+        thread_lprnet_post.start()
+        thread_remove_repeat_and_draw.start()
        
-    
     def decoder_and_pushdata(self, channel_list:list, multi_decoder:sail.MultiDecoder, PreProcessAndInference:sail.EngineImagePreProcess):
         """使用multidecoder进行多路解码, 解码后的图片将被直接push进入sail.EngineImagePreProcess进行预处理和推理
 
@@ -180,6 +191,7 @@ class MultiDecoderThread(object):
 
             while post_queue.full():
                 time.sleep(0.01)
+                logging.error("Thread 2 Inferences_thread, Post_queue is full!!!!!!!!!!!!!!")
                 if self.get_exit_flag():
                     break
                 continue
@@ -197,14 +209,12 @@ class MultiDecoderThread(object):
                         break
                     continue 
                 img_queue.put({(channel,imageidx_list[index]):ost_images[index]}) 
-
-                logging.debug("put ost img to queue, cid is {}, frameid is{}".format(channel,imageidx_list[index]))
+                logging.debug("Thread 2 Inferences_thread, Process {}, put ost img to queue, cid is {}, frameid is{}".format(self.process_id,channel,imageidx_list[index]))
 
             end_time = time.time()
-            logging.info("Engine_image_pre_process GetBatchData time use: {:.2f} ms".format((end_time-start_time)*1000))
+            logging.info("Thread 2 Inferences_thread,Process {} :Engine_image_pre_process GetBatchData time use: {:.2f} ms".format(self.process_id,(end_time-start_time)*1000))
         
         print("Inferences_thread thread exit!")
-
 
     def post_process(self, post_quque:queue.Queue, dete_threshold:float, nms_threshold:float):
         """通过post_quque得到模型推理后的数据, 并将数据传入后处理
@@ -234,19 +244,19 @@ class MultiDecoderThread(object):
                 if ret == 0:
                     break
                 else:
-                    print("push_data failed, ret: {}".format(ret))
+                    print("Thread 3 post_process, Process {},push_data failed, ret: {}".format(self.process_id,ret))
                     time.sleep(0.01)
                 break
-        print("post_process thread exit!")
+        print("Post_process thread exit!")
     
-    def lprnet_pre_and_process(self, img_queue:queue.Queue, ostimg_box_queue:queue.Queue):
+    def lprnet_pre_and_process(self, img_queue:queue.Queue, yolo_res_queue:queue.Queue):
         """从self.yolov5_post_async得到yolov5后处理的一组数据输出, 并将其在原图上crop出小图, 
         送入self.lprnet_engine_image_pre_process进行推理;
         并将yolov5后处理获取的数据送入ostimg_box_queue队列;
 
         Args:
             img_queue (queue.Queue): 存放原图的队列
-            ostimg_box_queue (queue.Queue): 存放原图和yolov5后处理结果队列
+            yolo_res_queue (queue.Queue): 存放原图和yolov5后处理结果队列
         """
         while (True):
             if self.get_exit_flag():
@@ -266,36 +276,35 @@ class MultiDecoderThread(object):
                 for obj in objs: # 一张图上多个结果
                     x1, y1, x2, y2, category_id, score = obj
                     img = list(ocv_image.values())[0]
-                    logging.debug("Lprnet_pre_and_process:Process {},channel_idx is {} image_idx is {},len(objs) is{}".format(self.process_id,channel, image_idx, len(objs)))
-                    logging.debug("Lprnet_pre_and_process:Process %d,YOLO postprocess DONE! objs:tuple[left, top, right, bottom, class_id, score] :%s",self.process_id,obj)
+                    logging.debug("Thread 4 Lprnet_pre_and_process:Process {},channel_idx is {} image_idx is {},len(objs) is{}".format(self.process_id,channel, image_idx, len(objs)))
+                    logging.debug("Thread 4 Lprnet_pre_and_process:Process %d,YOLO postprocess DONE! objs:tuple[left, top, right, bottom, class_id, score] :%s",self.process_id,obj)
 
                     croped = self.bmcv.crop(img,int(x1),int(y1),int(x2-x1),int(y2-y1))
                     self.lprnet_engine_image_pre_process.PushImage(channel, image_idx, croped)
 
 
-                    # put box and img to queue, for draw images
-                    if self.draw_images:
-                        while ostimg_box_queue.full() and not self.get_exit_flag(): 
-                            print("lprnet_pre_and_process put ost img to ostimg_box_queue is full!!!!!!!!!!!!")
-                            logging.dehbg("lprnet_pre_and_process put ost img to ostimg_box_queue is full!!!!!!!!!!!!")
-                            logging.error("IM WAITING!! ............")
-                            time.sleep(0.01)
-                        copy_bmimg = sail.BMImage(self.handle,img.height(),img.width(),img.format(), img.dtype())
-                        self.bmcv.image_copy_to(img,copy_bmimg)
-                        
-                        ostimg_box_queue.put(({(channel,image_idx):copy_bmimg},obj)) # 是这里有内存泄漏吗
+                # put box and img to queue, 
+                if self.draw_images or self.remove_duplicate_plates:
+                    while yolo_res_queue.full(): 
+                        if self.get_exit_flag():
+                            break
+                        # print(f"Thread 4 Lprnet_pre_and_process: Process {self.process_id}, put ost img to yolo_res_queue is full!!!!!!!!!!!!")
+                        logging.debug(f"Thread 4 Lprnet_pre_and_process: Process {self.process_id}, put ost img to yolo_res_queue is full!!!!!!!!!!!!")
+                        logging.error("IM WAITING!! ............")
+                    
+                    yolo_res_queue.put(((channel,image_idx),(objs,ocv_image)))
             else:
-                logging.error("lprnet_pre_and_process: yolo post result idx, is not equal to origin images idx:")
+                logging.error(f"Thread 4 Lprnet_pre_and_process: Process {self.process_id}, yolo post result idx, is not equal to origin images idx:")
                 logging.error((channel,image_idx) ,list(ocv_image.keys())[0])
 
         print("Lprnet_pre_and_process thread exit!")
 
-    def lprnet_post_and_draw_result(self, ostimg_box_queue:queue.Queue):
+    def lprnet_post(self, lp_res_queue:queue.Queue):
         """通过self.lprnet_engine_image_pre_process获取lprnet后处理的数据;
         通过ostimg_box_queue获取原图和box数据, 保存识别结果图
 
         Args:
-            ostimg_box_queue (queue.Queue): 
+            lp_res_queue (queue.Queue): 
         """
 
         start_time = time.time()
@@ -304,7 +313,7 @@ class MultiDecoderThread(object):
             if self.get_exit_flag():
                 break
             output, _, channel_list, image_idx_list,_ = self.lprnet_engine_image_pre_process.GetBatchData_Npy() 
-            logging.debug("Lprnet_post:Process {},channel_idx is {} image_idx is {}".format(self.process_id,channel_list, image_idx_list))
+            logging.debug("Thread 5 Lprnet_post: Process {},channel_idx is {} image_idx is {}".format(self.process_id,channel_list, image_idx_list))
 
             output_array = output[self.lprnet_output_names][:4]
 
@@ -323,28 +332,32 @@ class MultiDecoderThread(object):
                     pre_c = c
                 res.append("".join(no_repeat_blank_label))
             print(res)
-            logging.info('Process {}, LPRNET POSTPROCESS DONE,res{}'.format(self.process_id, res))
-
-            # draw images
-            if self.draw_images:
+            logging.info('Thread 5 Lprnet_post: Process {}, LPRNET POSTPROCESS DONE,res{}'.format(self.process_id, res))
+            
+            if self.remove_duplicate_plates:
                 for temp in range(len(res)):
-                    i = channel_list[temp]
-                    j = image_idx_list[temp]
-                    lp_chinese_line = res[temp]
+                    lp_res_queue.put(((channel_list[temp], image_idx_list[temp]),res[temp])) 
+                
+                # 存图
+                # # draw images
+                # if self.draw_images:
+                #     for temp in range(len(res)):
+                #         i = channel_list[temp]
+                #         j = image_idx_list[temp]
+                #         lp_chinese_line = res[temp]
 
-                    ocv_image, obj = ostimg_box_queue.get(True) # get ost_imgs and box from yolo post
+                #         ocv_image, obj = ostimg_box_queue.get(True) # get ost_imgs and box from yolo post
 
-                    
-                    cid_yolo_ori_img,fid_yolo_ori_img = list(ocv_image.keys())[0] # ocv img :{(cid,fid):sail.bmimg}
+                #         cid_yolo_ori_img,fid_yolo_ori_img = list(ocv_image.keys())[0] # ocv img :{(cid,fid):sail.bmimg}
 
-                    if (i,j) != (cid_yolo_ori_img,fid_yolo_ori_img):
-                        logging.error("lprnet_post_and_draw_result: not equal frame.must be something wrong")
-                        
-                    else:
-                        x1, y1, x2, y2, category_id, score = obj
-                        # 在这里内存泄漏？!!
-                        draw_lp((list(ocv_image.values())[0]).asmat(),x1, y1, x2, y2,score,lp_chinese_line,i,j,self.process_id,temp)
-                        ocv_image.clear() # 似乎不起作用
+                #         if (i,j) != (cid_yolo_ori_img,fid_yolo_ori_img):
+                #             logging.error("Thread 5 lprnet_post: not equal frame.must be something wrong")
+                            
+                #         else:
+                #             x1, y1, x2, y2, category_id, score = obj
+                #             # 在这里内存泄漏？!!
+                #             draw_lp((list(ocv_image.values())[0]).asmat(),x1, y1, x2, y2,score,lp_chinese_line,i,j,self.process_id,temp)
+                #             ocv_image.clear() # 似乎不起作用
 
             if self.loop_count <=  image_idx_list[-1]:
                 logging.info("LOOPS DONE")
@@ -361,13 +374,86 @@ class MultiDecoderThread(object):
 
         logging.info("Process {}:Loops{},Total time use: {} ms, avg_time{}, {} FPS".format(self.process_id, self.loop_count,time_use,avg_time,1000/avg_time))
         print("Process {}:Loops{},Total time use: {} ms, avg_time{}, {} FPS".format(self.process_id, self.loop_count,time_use,avg_time,1000/avg_time))
-        
+
         self.flag_lock.acquire()
         self.exit_flag = True
         self.flag_lock.release()
 
-def process_demo(draw_images,tpu_id, max_que_size, video_name_list, yolo_bmodel,lprnet_bmodel, loop_count, process_id,dete_threshold,nms_threshold):
-    process =  MultiDecoderThread(draw_images,tpu_id, video_name_list, sail.sail_resize_type.BM_PADDING_TPU_LINEAR, max_que_size, loop_count,process_id)
+    def remove_repeat_and_draw(self, lp_res_queue:queue.Queue, yolo_res_queue:queue.Queue, template_in_threshold:int = 4,template_out_thresh:int = 20):
+        """检测到重复车牌，只报告一个:
+        在所有进程读取结束后读取log并上报检测结果, 单独开一个线程进行读取,
+        """
+        file = open('lp_result.txt', 'a')
+        template_infos = {}
+        # up_list = []
+        # rm_list = []
+
+        while True:
+            if self.get_exit_flag():
+                break
+            start_time = time.time()
+            # print("remove repeat:",template_infos,rm_list)
+            # elements = list(yolo_res_queue.queue)
+            # print(elements)
+
+            yolo_res = yolo_res_queue.get(True) # ((),())
+            yolo_id, yolo_boxes_and_ostimg = yolo_res
+            logging.error(f"mytest{time.time() - start_time}")
+            print("mytest",time.time() - start_time)
+            
+            while True:
+                rm_list = []
+                if self.get_exit_flag():
+                    break
+                lp_res = lp_res_queue.get(True) # ((),)
+                lp_id, lp_name = lp_res
+                print(time.time() - start_time)
+                # print("id:",lp_id,yolo_id)
+
+                if lp_name in template_infos.keys():
+                    template_infos[lp_name]["in"]+=1
+                    if template_infos[lp_name]["in"]==template_in_threshold:
+                        # up_list.append(lp_res)      
+                        file.write(f"process{self.process_id}:recongized license plates {lp_res} \n")
+
+                        # if self.draw_images:
+                        #     objs, ocv_img = yolo_boxes_and_ostimg
+                        #     draw_lp()
+                else:
+                    template_infos[lp_name]={}
+                    template_infos[lp_name]["in"]=1
+                    if template_infos[lp_name]["in"]==template_in_threshold:
+                        # up_list.append(lp_res)
+                        file.write(f"process{self.process_id}:recongized license plates {lp_res} \n")
+                        
+                for key in template_infos.keys():
+                    if key != lp_name:
+                        if "out" in template_infos[key].keys():
+                            template_infos[key]["out"]+=1
+                            if template_infos[key]["out"]>=template_out_thresh:
+                                rm_list.append(key)  
+                                rm_lp = key        
+                        else:
+                            template_infos[key]["out"]=1
+                if len(rm_list) :
+                    for key in rm_list:
+                        # print(key,rm_list,template_infos)
+                        if key in template_infos:
+                            del template_infos[key]
+
+                if lp_id == yolo_id:
+                    break
+            end_time = time.time() - start_time
+            logging.error(f"end{end_time}")
+            print("end",end_time)
+        print("Lprnet_pre_and_process thread exit!")
+        # for i in up_list:
+        #     # 打开文件，如果文件不存在则创建
+        #     with  as file:
+        #         file.write(f"process{self.process_id}:recongized license plates {i}")
+        
+def process_demo(draw_images, remove_duplicate_plates, tpu_id, max_que_size, video_name_list, yolo_bmodel,lprnet_bmodel, loop_count, process_id,dete_threshold,nms_threshold):
+    process =  MultiDecoderThread(draw_images, remove_duplicate_plates, tpu_id, video_name_list, sail.sail_resize_type.BM_PADDING_TPU_LINEAR, max_que_size, loop_count,process_id)
     process.InitProcess(yolo_bmodel,lprnet_bmodel,dete_threshold,nms_threshold)
 
 
@@ -381,7 +467,8 @@ def argsparser():
     parser.add_argument('--yolo_bmodel', type=str, default='../models/yolov5s-licensePLate/BM1684/yolov5s_v6.1_license_3output_int8_4b.bmodel', help='path of bmodel')
     parser.add_argument('--lprnet_bmodel', type=str, default='../models/lprnet/BM1684/lprnet_int8_4b.bmodel', help='path of bmodel')
     parser.add_argument('--dev_id', type=int, default=0, help='tpu id')
-    parser.add_argument('--draw_images', type=bool, default=False, help='tpu id')
+    parser.add_argument('--draw_images', type=bool, default=False, help='save image')
+    parser.add_argument('--remove_duplicate_plates',type=bool, default=True,help='Duplicate plates detected, only one is reported')
     args = parser.parse_args()
     return args
 
@@ -398,7 +485,7 @@ if __name__ == '__main__':
     input_videos = [args.input for _ in range(int(args.video_nums/process_nums))] # 初始化多路本地视频流
 
 
-    decode_yolo_processes = [Process(target=process_demo,args=(args.draw_images,args.dev_id, max_que_size, input_videos, args.yolo_bmodel,args.lprnet_bmodel, loop_count, i,dete_threshold,nms_threshold)) for i in range(process_nums) ]
+    decode_yolo_processes = [Process(target=process_demo,args=(args.draw_images, args.remove_duplicate_plates, args.dev_id, max_que_size, input_videos, args.yolo_bmodel,args.lprnet_bmodel, loop_count, i,dete_threshold,nms_threshold)) for i in range(process_nums) ]
     for i in decode_yolo_processes:
         i.start()
         logging.debug('start decode and yolo process')
@@ -411,4 +498,5 @@ if __name__ == '__main__':
 
     total_time = time.time() - start_time
     logging.info('video nums{}, process is {},total time is {},loops for one process is {},total fps is {}'.format(args.video_nums,process_nums,total_time,loop_count,(loop_count*process_nums)/total_time))
+    print('video nums{}, process is {},total time is {},loops for one process is {},total fps is {}'.format(args.video_nums,process_nums,total_time,loop_count,(loop_count*process_nums)/total_time))
 
